b775519addb1e88aad2e939b47dc126cccbb39f4
arch <aa>
2019-03-29 22:25:04 +0100

    Add tdidf

--- src/main/java/FeatureExtractor.java
+++ src/main/java/FeatureExtractor.java
@@ -26,6 +26,54 @@ public class FeatureExtractor {
 
         return temp;
     }
+
+    static Map lengExtractor(List <String> text) {
+
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
+        temp.put("leng", text.size());
+        return temp;
+    }
+
+    static Map wordPositionExtractor(List <String> text, List <String> model) {
+
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
+
+        for(String word : model) {
+            if(text.contains(word)){
+                temp.put(word, text.indexOf(word));
+            }
+
+        }
+        return temp;
+    }
+
+    static Map TfIdf (List <String> text, List <String> model, List<List<String>> corpus){
+        HashMap<String, Double> temp = new LinkedHashMap<>();
+
+        int textLeng = text.size();
+
+        for(String word : model) {
+            if(text.contains(word)) {
+
+                double tf = Collections.frequency(text, word) / textLeng;
+
+                int documentsWithTerm = 0;
+
+                for(List document: corpus){
+                    if(document.contains(word)){
+                        documentsWithTerm++;
+                    }
+                }
+
+                double idf = Math.log(corpus.size() / documentsWithTerm);
+
+
+                temp.put(word, tf * idf);
+            }
+        }
+
+        return temp;
+    }
 }
 
 
