5ce4d0630043eacaf0449275b0abf7c07824deb3
arch <aa>
2019-04-03 19:44:54 +0200

    clear

--- src/main/java/Vectorizer.java
+++ src/main/java/Vectorizer.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Vectorizer {
 
     public static List<String> string2vec(String text){
+
         return Arrays.asList(text.split("\\s+"));
+
     }
 
     public static HashMap<String, Integer> generateCorpus( List<List<String>> documents){
@@ -11,29 +13,22 @@ public class Vectorizer {
 
         for(List<String> doc: documents){
             for(String word: doc){
-
                 if(corpus.containsKey(word)){
                     corpus.put(word, corpus.get(word) + 1);
                 }
                 else{
                     corpus.put(word, 1);
                 }
-
             }
         }
-
         return corpus;
     }
 
 
     public static Map<String, Integer> sortByValue(Map<String, Integer> corpus)
     {
-
         List<Map.Entry<String, Integer> > list = new LinkedList<>(corpus.entrySet());
-
-
         list.sort(Comparator.comparing(o -> (o.getValue())));
-
 
         HashMap<String, Integer> temp = new LinkedHashMap<>();
 
