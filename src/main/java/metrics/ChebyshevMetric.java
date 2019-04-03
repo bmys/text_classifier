06c94f9b73416d5136b69c0013eb57b359b97554
arch <aa>
2019-04-03 19:35:54 +0200

    create metrics boilerplate

---
+++ src/main/java/metrics/ChebyshevMetric.java
@@ -0,0 +1,10 @@
+package metrics;
+
+import java.util.List;
+
+public class ChebyshevMetric implements iMetric{
+    @Override
+    public double getDistance(List a, List b) {
+        return 0;
+    }
+}
---
+++ src/main/java/metrics/TaxiCabMetric.java
@@ -0,0 +1,10 @@
+package metrics;
+
+import java.util.List;
+
+public class TaxiCabMetric implements iMetric {
+    @Override
+    public double getDistance(List a, List b) {
+        return 0;
+    }
+}
