e277be6517a1e8de6ba82f11e48e65be8af06030
arch <aa>
2019-04-07 01:32:55 +0200

    Add setFeature to document Create predictor class

---
+++ src/main/java/Knn.java
@@ -0,0 +1,49 @@
+import metrics.iMetric;
+import model.Corpus;
+import org.jsoup.nodes.Document;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Knn {
+    private int k;
+    private iMetric metric;
+    private model.Corpus corpus;
+
+    public Knn(int k, iMetric metric, Corpus corpus) {
+        this.k = k;
+        this.metric = metric;
+        this.corpus = corpus;
+    }
+
+    public String predict(model.Document doc){
+        List<String> features = new ArrayList<>(doc.getFeatures().keySet());
+        List<Double> a = new LinkedList<>();
+
+        for(String feature: features){
+            a.add(doc.getFeatures().get(feature));
+        }
+
+        Map<model.Document, Double> distanceToDocuments= new HashMap<>();
+
+        for(model.Document dc: corpus.getDocuments()){
+            List<Double> b = new LinkedList<>();
+            for(String feature: features){
+                b.add(dc.getFeatures().get(feature));
+            }
+            double distance = this.metric.getDistance(a, b);
+
+            distanceToDocuments.put(dc, distance);
+        }
+
+        distanceToDocuments = Vectorizer.sortByValued(distanceToDocuments);
+
+        List<Map.Entry<model.Document, Double>> predict = new LinkedList(distanceToDocuments.entrySet());
+
+        for(int i=0; i<k; i++){
+            System.out.println(predict.get(i).getKey().getLabels());
+        }
+
+        return "xD";
+    }
+}
