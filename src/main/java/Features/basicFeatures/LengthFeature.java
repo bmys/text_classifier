ff233efb93d6bb833c70c49af55a2623c3af6ce3
bmys <bmys94@gmail.com>
2019-05-22 00:15:41 +0200

    add LoadSGML class, LengthFeature

---
+++ src/main/java/Features/basicFeatures/LengthFeature.java
@@ -0,0 +1,16 @@
+package Features.basicFeatures;
+
+import Features.FeatureExtractor;
+import Model.Document;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+
+public class LengthFeature implements FeatureExtractor<Double> {
+
+  @Override
+  public Entry<String, Double> extract(Document document) {
+
+    String name = "keywordsDensity";
+    return new SimpleEntry<>(name, (double) document.getTokens().size());
+  }
+}
---
+++ src/main/java/Utility/DataLoader/LoadSGML.java
@@ -0,0 +1,77 @@
+package Utility.DataLoader;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class LoadSGML {
+
+  static Document loadSgml(String path) throws IOException {
+
+    File input = new File(path);
+    Scanner s = new Scanner(input).useDelimiter("\\A");
+    String result = s.hasNext() ? s.next() : "";
+    result = result.replace("<BODY", "<INNER");
+    return Jsoup.parse(result);
+  }
+
+  public static List<Model.Document> loadFromDir(String path) throws IOException {
+    List<Model.Document> articles = new ArrayList<>();
+
+    File dir = new File(path);
+
+    File[] files = dir.listFiles((dir1, name) -> name.endsWith(".sgm"));
+
+    assert files != null;
+
+    Arrays.sort(files);
+
+    for (File file : files) {
+      Document doc = loadSgml(file.toString());
+      List<Model.Document> articlesFromDoc = loadArticlesWithLocation(doc);
+      System.out.println(articlesFromDoc.size());
+      articles.addAll(articlesFromDoc);
+    }
+    return articles;
+  }
+
+  public static List<Model.Document> filterLocation(List<Model.Document> articles,
+      List<String> locations) {
+    List<Model.Document> filteredArticles = new ArrayList<>();
+
+    for (Model.Document art : articles) {
+      List loc = art.getLabels().get("locations");
+
+      if (loc.size() == 1 && locations.contains(loc.get(0))) {
+        filteredArticles.add(art);
+      }
+    }
+    return filteredArticles;
+  }
+
+  public static List<Model.Document> loadArticlesWithLocation(Document doc) {
+    List<Model.Document> articles = new ArrayList<>();
+
+    Elements reuters = doc.select("REUTERS");
+
+    for (Element elem : reuters) {
+      String title = elem.select("TITLE").text();
+      String text = elem.select("INNER").text();
+
+      String places = elem.select("PLACES > D").text();
+      List<String> placesList = new ArrayList<>(Arrays.asList(places.split("\\s+")));
+
+      Model.Document art = new Model.Document();
+      articles.add(art);
+    }
+    return articles;
+  }
+}
