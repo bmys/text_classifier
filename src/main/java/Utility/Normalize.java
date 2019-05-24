1bee119e286314a90f66851f2af3b1381ef986fe
bmys <bmys94@gmail.com>
2019-05-24 20:11:55 +0200

    create vector normalizer

--- src/main/java/Utility/Normalize.java
+++ src/main/java/Utility/Normalize.java
@@ -4,8 +4,17 @@ import com.google.common.collect.Iterables;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Normalize {
+
+  public static Map<String, Double> normalize(
+      Map<String, Double> toNormalize, Map<String, Double> maxValues) {
+
+    return toNormalize.entrySet().stream()
+        .collect(Collectors.toMap(Entry::getKey, x -> x.getValue() / maxValues.get(x.getKey())));
+  }
 
   public static Map<String, Double> findBiggestInMap(List<Map<String, Double>> vectors) {
     Map<String, Double> biggest = new HashMap<>(vectors.get(0));
--- src/test/java/Utility/NormalizeTest.java
+++ src/test/java/Utility/NormalizeTest.java
@@ -10,29 +10,38 @@ import org.junit.Test;
 
 public class NormalizeTest {
 
+  private HashMap<String, Double> map1 = new HashMap<String, Double>() {{
+    put("a", 3.0);
+    put("b", 2.0);
+    put("c", 1.0);
+  }};
+
+  private HashMap<String, Double> map2 = new HashMap<String, Double>() {{
+    put("a", 1.0);
+    put("b", 3.0);
+    put("c", 2.0);
+  }};
+
+  private HashMap<String, Double> map3 = new HashMap<String, Double>() {{
+    put("a", 4.0);
+    put("b", 2.0);
+    put("c", 2.0);
+  }};
+
+  private List<Map<String, Double>> mapList = new ArrayList<>(Arrays.asList(map1, map2, map3));
+
+  @Test
+  public void normalize() {
+    Map<String, Double> bgmp = Normalize.findBiggestInMap(mapList);
+    Map<String, Double> normalized = Normalize.normalize(map1, bgmp);
+
+    Assert.assertEquals(0.75, normalized.get("a"), 0.01);
+    Assert.assertEquals(0.66, normalized.get("b"), 0.01);
+    Assert.assertEquals(0.5, normalized.get("c"), 0.01);
+  }
+
   @Test
   public void largestElementsInMapTest() {
-
-    HashMap<String, Double> map1 = new HashMap<String, Double>() {{
-      put("a", 3.0);
-      put("b", 2.0);
-      put("c", 1.0);
-    }};
-
-    HashMap<String, Double> map2 = new HashMap<String, Double>() {{
-      put("a", 1.0);
-      put("b", 3.0);
-      put("c", 2.0);
-    }};
-
-    HashMap<String, Double> map3 = new HashMap<String, Double>() {{
-      put("a", 4.0);
-      put("b", 2.0);
-      put("c", 2.0);
-    }};
-
-    List<Map<String, Double>> mapList = new ArrayList<>(Arrays.asList(map1, map2, map3));
-
     Map<String, Double> bgmp = Normalize.findBiggestInMap(mapList);
 
     Assert.assertEquals(bgmp.get("a"), 4.0, 0.001);
